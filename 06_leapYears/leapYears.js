7898b56e1baa70a69fc8d9a13986501ae7737b20
Sebastian <sebastianhothaza@gmail.com>
2022-10-17 21:55:16 -0400

    Completed up to problem 7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year%4)){ //If divisble by 4
+        if (!(year%400)){ //if divisible by 400
+            return true;
+        }
+        if (!(year%100)){ //if divisible by 100
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
